ef18e3d76781ce5ed6501ad947b095dd54593f72
IsabelaHYGS <168226667+IsabelaHYGS@users.noreply.github.com>
2025-06-10 11:33:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
@@ -11,6 +12,12 @@ function mostraPalavrasChave() {
 } 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+    palavras = tiraPalavrasRuins(palavras);
+
+
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
@@ -35,6 +42,19 @@ function contaFrequencias(palavras) {
     }
   
     return frequencias;
+  }
+  function tiraPalavrasRuins(palavras) {
+   
+    const palavrasBoas = [];
+  
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+    
+      }
+    }
+  
+    return palavrasBoas;
   }
   
   
